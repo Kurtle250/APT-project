97819061d83a90c44a72a63da90b3f0d56f1849d
Kurtle250 <kurt.g.elliott@gmail.com>
2023-04-07 08:13:54 -0600

    testing github action

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from apt_decoder import apt_decoder
 
 
 def main():
-    app = apt_decoder("sample16.wav")
+    #/home/kurt/downloads/jp1odj-air.proxy.kiwisdr.com_2023-04-07T03_19_00Z_137912.00_iq.wav
+    app = apt_decoder("/home/kurt/Downloads/sample.wav")
     #app.display_plot()
     # testing CICD
     app.display_image()
